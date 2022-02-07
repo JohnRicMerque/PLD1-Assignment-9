304c51a53e36d86d03057846c96d77b62a6bcd2a
unknown <you@example.com>
2022-02-07 23:25:50 +0800

    sucessfully added image and displayed details

--- resume_creator.py
+++ resume_creator.py
@@ -13,18 +13,32 @@ with open('details.json') as dataFile:
 # retrieving data from json file
 # Personal Information
 name = resumeDetails["name"]
+age = resumeDetails["age"]
 address = resumeDetails["address"]
+
 # Contact Information
 number = resumeDetails["contactNumber"]
 
 # creating pdf
-resume = FPDF("P", "cm",)
+resume = FPDF('P', 'cm', 'A4')
 resume.add_page()
-resume.set_font("Arial", "B", 18)
-resume.set_fill_color(181,252,201)
 
-resume.cell(0, 5, "              " + name, ln=1, align="L", fill=1)
-#pdf.image('idpic.jpg', 15, 1, 5)
+# customizing header
+resume.set_font("Arial", "B", 25)
+resume.set_text_color(48,51,49)
+resume.set_fill_color(181,252,201)
+resume.cell(20, 5,name, ln=1, align="C", fill=1)
+resume.image('resumepic.jpg',0, 1, 5)
+
+# inserting personal information details
+resume.set_text_color(255,255,255)
+resume.set_fill_color(48,51,49)
+resume.set_font("Arial", "", 18)
+resume.cell(20,1, "Personal Information", align="L", ln=1, fill=1)
+# specific details
+resume.set_font_size(14)
+resume.cell(0,0.75, "age: " + age, align="L", ln=1, fill=0)
+resume.cell(0,0.75, "address: " + address, align="L", ln=1, fill=0)
 
 # output pdf
 resume.output("MERQUE_JOHNRIC.pdf")
